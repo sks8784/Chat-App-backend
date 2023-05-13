061bdcec774b99429cbb17cce1048acfcf466031
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-05-13 20:11:51 +0530

    made some changes

--- index.js
+++ index.js
@@ -2,27 +2,24 @@ const http=require("http");
 const express=require("express");
 const cors=require("cors");
 const socketIO=require("socket.io");
-const { Server } = require('socket.io');
+// const { Server } = require('socket.io');
 
 const app=express();
-const port=4500 || process.env.PORT;
+
+app.use(cors());
+const port= 4500 || process.env.PORT;
 
 const users=[{}];
 
-app.use(cors());
+
 app.get("/",(req,res)=>{
     res.send("Hello World");
 }) 
 
 const server=http.createServer(app);
 
-// const io=socketIO(server);
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ['GET', 'POST'],
-    }
-})
+const io=socketIO(server);
+
 
 io.on("connection",(socket)=>{
     console.log("new connection");

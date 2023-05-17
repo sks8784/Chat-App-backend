63022d89145d90bdbce2742906fe758a903dd553
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-05-17 18:04:54 +0530

    change made

--- database/db.js
+++ database/db.js
@@ -1,14 +1,16 @@
-const mongoose=require('mongoose');
-const connectDB=async()=>{
-    try{
-        const conn=await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser:true,
+const mongoose = require('mongoose');
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         });
         console.log('MongoDB connected');
-    }catch(err){
+    } catch (err) {
         console.log(err);
         process.exit(1);
     }
 }
 
-module.exports={connectDB};
+module.exports = { connectDB };

9933873b4cd7dfcf3b3e35dbe85eda519d3a2ebb
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-05-17 18:08:25 +0530

    new changes made

--- database/db.js
+++ database/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL, {
-            useCreateIndex: true,
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
